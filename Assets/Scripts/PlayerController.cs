093d99aaefc309a3b7c2acd31e4d961ea63a9968
Maxim <maximsharypov@gmail.com>
2021-10-25 23:36:56 +0700

    Управление и генерация префабов

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 dir;
+    [SerializeField] private int speed;
+
+    private int lineToMove = 1;
+    public float lineDistance = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (SwipeController.swipeRight)
+        {
+            if (lineToMove < 1)
+                lineToMove++;
+        }
+
+        if (SwipeController.swipeLeft)
+        {
+            if (lineToMove > 0)
+                lineToMove--;
+        }
+
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+        if (lineToMove == 0)
+            targetPosition += Vector3.left * lineDistance;
+        else if (lineToMove == 1)
+            targetPosition += Vector3.right * lineDistance;
+
+        transform.position = targetPosition;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        dir.z = speed;
+        controller.Move(dir * Time.fixedDeltaTime);
+    }
+}
+
